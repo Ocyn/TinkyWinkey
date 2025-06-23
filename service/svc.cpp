c9285e6c15a1016f980324adb7ac4fe516a67144
naterrie <naterrie@student.42lyon.fr>
2025-06-23 16:53:26 +0200

    feat: implement service management functions and improve output formatting

--- service/svc.cpp
+++ service/svc.cpp
@@ -1,5 +1,25 @@
 #include "svc.hpp"
 
+int InstallService()
+{
+	return 0;
+}
+
+int StartService()
+{
+	return 0;
+}
+
+int StopService()
+{
+	return 0;
+}
+
+int DeleteService()
+{
+	return 0;
+}
+
 int _tmain(int arc, TCHAR *argv[])
 {
 	if (arc < 2)
@@ -9,23 +29,47 @@ int _tmain(int arc, TCHAR *argv[])
 	}
 	if (_tcscmp(argv[1], _T("install")) == 0)
 	{
-		_tprintf(_T("Installing service..."));
+		_tprintf(_T("Installing service...\n"));
+		if (!InstallService())
+		{
+			_tprintf(_T("Failed to install service.\n"));
+			return 1;
+		}
+		_tprintf(_T("Service installed successfully.\n"));
 	}
 	else if (_tcscmp(argv[1], _T("start")) == 0)
 	{
-		_tprintf(_T("Starting service..."));
+		_tprintf(_T("Starting service...\n"));
+		if (!StartService())
+		{
+			_tprintf(_T("Failed to start service.\n"));
+			return 1;
+		}
+		_tprintf(_T("Service started successfully.\n"));
 	}
 	else if (_tcscmp(argv[1], _T("stop")) == 0)
 	{
-		_tprintf(_T("Stopping service..."));
+		_tprintf(_T("Stopping service...\n"));
+		if (!StopService())
+		{
+			_tprintf(_T("Failed to stop service.\n"));
+			return 1;
+		}
+		_tprintf(_T("Service stopped successfully.\n"));
 	}
 	else if (_tcscmp(argv[1], _T("delete")) == 0)
 	{
-		_tprintf(_T("Deleting service..."));
+		_tprintf(_T("Deleting service...\n"));
+		if (!DeleteService())
+		{
+			_tprintf(_T("Failed to delete service.\n"));
+			return 1;
+		}
+		_tprintf(_T("Service deleted successfully.\n"));
 	}
 	else
 	{
-		_tprintf(_T("Unknown option: %s"), argv[1]);
+		_tprintf(_T("Unknown option: %s\n"), argv[1]);
 		return 1;
 	}
 }
