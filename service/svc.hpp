88e0cbfab63e0cbc8994c9620cdac719a93d6d6f
Ocyn <niko.ocyn@gmail.com>
2025-06-24 12:52:14 +0200

    feat: add service control functions and global variables in svc.hpp

--- service/svc.hpp
+++ service/svc.hpp
@@ -8,4 +8,21 @@
 #include <strsafe.h>
 #include <stdio.h>
 
+#pragma comment(lib, "Advapi32.lib")
+
+// Main service function
+VOID WINAPI ServiceMain(DWORD argc, LPTSTR *argv);
+VOID WINAPI ServiceCtrlHandler(DWORD);
+DWORD WINAPI ServiceWorkerThread(LPVOID lpParam);
+
+
+// Global variables for service control
+SERVICE_STATUS g_ServiceStatus = {0};
+SERVICE_STATUS_HANDLE g_StatusHandle = NULL;
+HANDLE g_ServiceStopEvent = INVALID_HANDLE_VALUE;
+PROCESS_INFORMATION g_ProcessInfo = {0};
+
+// Service name definition
+#define SERVICE_NAME _T("svc_fdp")
+
 #endif
